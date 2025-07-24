e763146a683616af3bbe83164c951571ab093b53
artuasova <a9173475441@gmail.com>
2025-07-24 17:21:13 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'smoking-counter-v1';
+const urlsToCache = [
+  '/smoking-counter/',
+  '/smoking-counter/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
